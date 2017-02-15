368a36a8b2803f99c1832b8ad2d72204cc5c6918
Serhii <serhyy.kovalenko@i.ua>
2017-02-15 20:19:46 +0200

    Fix leak using return value

--- src/c/text_factory.h
+++ src/c/text_factory.h
@@ -1,5 +1,5 @@
 #pragma once
 
 void set_text_by_row(int row, TextLayer * text_layer);
-void set_image_by_row(int row, BitmapLayer *image_layer, GBitmap **content_image);
+GBitmap* get_image_by_row(int row);
 void draw_row_callback (GContext *ctx, Layer *cell_layer, MenuIndex *cell_index, void *callback_context);
